10d187b000152471fdbe6a3f1dcec91d9417c5b9
liliccc <xianj.chen@qq.com>
2022-01-31 21:49:28 +0800

    1.31

---
+++ src/algorithm/oneWayRecursion/DoubleFactorialNonRecursion.java
@@ -0,0 +1,23 @@
+package algorithm.oneWayRecursion;
+
+public class DoubleFactorialNonRecursion {
+    /**
+     * @param n: the given number
+     * @return:  the double factorial of the number
+     */
+    public long doubleFactorial(int n) {
+        return doubleFactorial(n, 1);
+    }
+
+    private long doubleFactorial(int n, long result) {
+        while(true) {
+            if (n <= 2) {
+                return n * result;
+            }
+            long nextResult = n * result;
+            int nextN = n - 2;
+            n = nextN;
+            result = nextResult;
+        }
+    }
+}
